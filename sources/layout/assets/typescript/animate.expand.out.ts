a4196c5274eeaa42753995a99433b06beb07d786
Jean Franceszco Jimenez I'm a Developer UI/UX <j_juanfrancisco_jmz@yahoo.com.mx>
2022-03-28 17:26:20 -0600

    :heavy_plus_sign: :man_juggling: :carousel: Add carousel component and its effect(s).

--- sources/layout/assets/typescript/animate.expand.out.ts
+++ sources/layout/assets/typescript/animate.expand.out.ts
@@ -1,11 +1,11 @@
 window.addEventListener('scroll', () => {
     const avatarImage = document.querySelector('.complete-avatar');
     const aboutme = document.querySelector('.about-me');
-    const card = aboutme.querySelector('.card');
+    const cards = aboutme.querySelectorAll('.card');
     const top = aboutme.getBoundingClientRect().top;
 
     const setExpandable = (window.scrollY > 43 && top > -20);
 
     avatarImage.classList.toggle('expand-out', setExpandable);
-    card.classList.toggle('expand-out', setExpandable);
+    cards.forEach(card => card.classList.toggle('expand-out', setExpandable) );
 });
---
+++ sources/layout/assets/typescript/carousel.effect.ts
@@ -0,0 +1,52 @@
+interface Carrousel {
+    currentActiveIndex: number,
+    activeClass: string,
+    items: NodeListOf<Element>,
+    updateCurrentActive: (offset: number) => void,
+};
+
+function setClass(htmlElem: Element, cssClass: string){
+    htmlElem.classList.add(cssClass);
+}
+
+function removeClass(htmlElem: Element, cssClass: string) {
+    htmlElem.classList.remove( cssClass );
+}
+
+function calculateNextIndex(currentIndex: number, offset: number, numberOfItems: number) {
+    return ( currentIndex + offset ) % numberOfItems;
+}
+
+function updateCurrentActive(offset: number) {
+    const nextCurrentActive: number = calculateNextIndex(this.currentActiveIndex, offset, this.items.length);
+
+    removeClass(this.items[ this.currentActiveIndex ], this.activeClass);
+    setClass(this.items[ nextCurrentActive ], this.activeClass);
+
+    this.currentActiveIndex = nextCurrentActive;
+}
+
+function triggerCarrouselChange(carrousel: Carrousel, nextOrBack: 'Next' | 'Back' = 'Next') {
+    switch( nextOrBack ) {
+        case 'Next':
+            carrousel.updateCurrentActive(+1);
+            break;
+        case 'Back':
+            carrousel.updateCurrentActive(-1);
+    }
+}
+
+function launchCarrouselTimmer(carrousel: Carrousel, miliseconds: number = 2000) {
+    setInterval( () => triggerCarrouselChange(carrousel), miliseconds );
+}
+
+window.addEventListener('load', () => {
+    const carrousel: Carrousel = {
+        currentActiveIndex: 0,
+        activeClass: 'slide-active',
+        items: document.querySelectorAll('#about-me .carousel > .carousel-item'),
+        updateCurrentActive: updateCurrentActive,
+    };
+
+    launchCarrouselTimmer(carrousel);
+});
