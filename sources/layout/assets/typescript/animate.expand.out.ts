827cc1d689ae254f3f525b09f244a8225b68ac05
Jean Franceszco Jimenez I'm a Developer UI/UX <j_juanfrancisco_jmz@yahoo.com.mx>
2023-02-10 16:24:04 -0600

    :art: :performing_arts: :man_juggling: Try to Fix the animation effect of the carrousel + avatar image in the about me section.

--- sources/layout/assets/typescript/animate.expand.out.ts
+++ sources/layout/assets/typescript/animate.expand.out.ts
@@ -4,8 +4,9 @@ window.addEventListener('scroll', () => {
     const cards = aboutme.querySelectorAll('.card');
     const top = aboutme.getBoundingClientRect().top;
 
-    const setExpandable = (window.scrollY > 43 && top > -20);
+    const setExpandableImg = (window.scrollY > 5 && top > -45);
+    const setExpandableCard = (window.scrollY > 33 && top > -130);
 
-    avatarImage.classList.toggle('expand-out', setExpandable);
-    cards.forEach(card => card.classList.toggle('expand-out', setExpandable) );
+    avatarImage.classList.toggle('expand-out', setExpandableImg);
+    cards.forEach(card => card.classList.toggle('expand-out', !setExpandableCard) );
 });
