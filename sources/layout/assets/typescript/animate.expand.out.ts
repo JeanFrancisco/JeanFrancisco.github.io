0b6a1532ec35decc7502f0cbedf191c018fe3683
Jean Franceszco Jimenez I'm a Developer UI/UX <j_juanfrancisco_jmz@yahoo.com.mx>
2023-02-10 18:18:41 -0600

    :art: :performing_arts: :man_juggling: Major Perfomming & Improve UI/UX and Look nd Feel. Add SVG Avatar images. Define about me, profile typography.

--- sources/layout/assets/typescript/animate.expand.out.ts
+++ sources/layout/assets/typescript/animate.expand.out.ts
@@ -5,7 +5,7 @@ window.addEventListener('scroll', () => {
     const top = aboutme.getBoundingClientRect().top;
 
     const setExpandableImg = (window.scrollY > 5 && top > -45);
-    const setExpandableCard = (window.scrollY > 33 && top > -130);
+    const setExpandableCard = (window.scrollY > 23 && top > -110);
 
     avatarImage.classList.toggle('expand-out', setExpandableImg);
     cards.forEach(card => card.classList.toggle('expand-out', !setExpandableCard) );
